82ab9b10c65c4b94e3b45589d6bfc0b41287a134
dothanhhxx <dothanhhai281@gmail.com>
2026-03-31 16:57:48 +0700

    Update seed_data.py

--- backend/seed_data.py
+++ backend/seed_data.py
@@ -9,7 +9,7 @@ DB_CONFIG = {
     "host": "127.0.0.1",
     "port": 3306,
     "user": "root",
-    "password": "12345",
+    "password": "123456",
     "database": "LearnUp",
     "charset": "utf8mb4",
 }
