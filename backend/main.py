a0ca1b81244853df9ee94007857e246027d2caab
NamLe218 <tranlenam21805@gmail.com>
2026-04-05 04:22:54 +0700

    Revert "Dashboard UI"
    This reverts commit f9ef696e807b57f2af8a8996ecbd84fbd9f50c32.

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
 app.include_router(users.router)
 app.include_router(articles.router)
 
+
 @app.get("/api/health")
 def health_check():
     return {"status": "OK", "message": "LearnUp API is running (FastAPI)."}
