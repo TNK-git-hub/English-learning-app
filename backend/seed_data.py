04cf393420b2f2b0f01929ec6fd171bf8573508f
TNK-git-hub <khanhpersonalwork@gmail.com>
2026-04-10 21:17:47 +0700

    run web

--- backend/seed_data.py
+++ backend/seed_data.py
@@ -12,13 +12,6 @@ load_dotenv()
 
 
 def seed():
-    DB_CONFIG = {
-        "host": os.getenv("DB_HOST", "127.0.0.1"),
-        "user": os.getenv("DB_USER", "root"),
-        "password": os.getenv("DB_PASSWORD", "123456"),
-        "database": os.getenv("DB_NAME", "LearnUp"),
-        "port": int(os.getenv("DB_PORT", 3306))
-    }
     conn = mysql.connector.connect(**DB_CONFIG)
     cursor = conn.cursor()
 
